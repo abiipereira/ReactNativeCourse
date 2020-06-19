3d0fea89481d572a150de9bbbecacc82b6bfd994
abiipereira <abiipereira@gmail.com>
2020-06-19 23:25:23 +0530

    Gestures Part 2 with Rubberband

--- components/DIshdetailComponent.js
+++ components/DIshdetailComponent.js
@@ -22,7 +22,9 @@ function RenderDish(props) {
    // console.log(props);
     const dish = props.dish;
 
-    //handleViewRef = ref => this.view = ref;
+    var viewRef;
+
+    const handleViewRef = ref => viewRef = ref;
 
     const recognizeDrag = ({ moveX, moveY, dx, dy }) => {
         if ( dx < -200 )
@@ -35,7 +37,7 @@ function RenderDish(props) {
         onStartShouldSetPanResponder: (e, gestureState) => {
             return true;
         },
-        //onPanResponderGrant: () => {this.view.rubberBand(1000).then(endState => console.log(endState.finished ? 'finished' : 'cancelled'));},
+        onPanResponderGrant: () => {viewRef.rubberBand(1000).then(endState => console.log(endState.finished ? 'finished' : 'cancelled'));},
         onPanResponderEnd: (e, gestureState) => {
             console.log("pan responder end", gestureState);
             if (recognizeDrag(gestureState))
@@ -57,7 +59,7 @@ function RenderDish(props) {
             return(
                 <View>
                 <Animatable.View animation="fadeInDown" duration={2000} delay={1000} 
-                //ref={this.handleViewRef} 
+                ref={handleViewRef} 
                 {...panResponder.panHandlers}>
                 <Card
                 featuredTitle={dish.name}
