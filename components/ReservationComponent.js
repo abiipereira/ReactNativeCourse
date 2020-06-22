9e403ea84b11855b14e35ed835c4b177ff07459a
abiipereira <abiipereira@gmail.com>
2020-06-22 15:39:50 +0530

    Local Notifications faulty

--- components/ReservationComponent.js
+++ components/ReservationComponent.js
@@ -3,7 +3,8 @@ import { Text, View, ScrollView, StyleSheet, Picker, Switch, Button, Modal, Aler
 import { Card } from 'react-native-elements';
 import DatePicker from 'react-native-datepicker'
 import * as Animatable from 'react-native-animatable';
-
+import * as Permissions from 'expo-permissions';
+//import * as Notifications  from 'expo-notifications';
 class Reservation extends Component {
 
     constructor(props) {
@@ -37,6 +38,7 @@ class Reservation extends Component {
                 },
                 {
                     text: 'OK',
+                    //this.presentLocalNotification(this.state.date) SOS
                     onPress: () => this.resetForm(),
                 }
             ],
@@ -57,7 +59,33 @@ class Reservation extends Component {
         });
     }
     
-    
+     async obtainNotificationPermission() {
+        let permission = await Permissions.getAsync(Permissions.USER_FACING_NOTIFICATIONS);
+        if (permission.status !== 'granted') {
+            permission = await Permissions.askAsync(Permissions.USER_FACING_NOTIFICATIONS);
+            if (permission.status !== 'granted') {
+                Alert.alert('Permission not granted to show notifications');
+            }
+        }
+        return permission;
+    }
+
+    async presentLocalNotification(date) {
+        await this.obtainNotificationPermission();
+        Notifications.presentLocalNotificationAsync({
+            title: 'Your Reservation',
+            body: 'Reservation for '+ date + ' requested',
+            ios: {
+                sound: true
+            },
+            android: {
+                sound: true,
+                vibrate: true,
+                color: '#512DA8'
+            }
+        });
+    }
+
     render() {
         return(
             <ScrollView>
